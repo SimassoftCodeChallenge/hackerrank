ad5c9d6b80c462a27fbfe630ecbf8682e4b87078
Gabriel Simas <autorgabrielsimas@gmail.com>
2017-06-17 19:52:20 -0300

    Desafio ArrayDS enviado com sucesso. 10.000 pontos recebidos.

---
+++ HackerRankChallenges/ArraysDs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numArray = 0;
+            string numeros = string.Empty;            
+
+            Console.Clear();
+
+            numArray = int.Parse(Console.ReadLine());
+            numeros = Console.ReadLine();
+            string[] resultado = new string[numArray];
+            resultado = numeros.Split(' ');
+            Array.Reverse(resultado);            
+
+            Console.WriteLine(string.Join(" ",resultado.ToArray()));            
+        }
+    }
+}
